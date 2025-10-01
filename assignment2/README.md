65531c95fb4ffac43cfca538f6d0f1654b959146
YuukaLi-yl12624 <yl12624@nyu.edu>
2025-10-01 09:29:08 -0400

    week4 +1

--- assignment2/README.md
+++ assignment2/README.md
@@ -1,61 +1,15 @@
-# template
+# Assignment 2
 
 ---
 
-This is a README file that can be used to describe and document your assignment.
-
-Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
-
 ---
 
-# Heading1
-## Heading2
-### Heading3
-#### Heading4
-##### Heading5
-###### Heading6
+# Generative Pattern Assignment
 
-**bold text**
+I chose to make a raindrop effect for this assignment.
 
-*italicized text*
+I made the raindop smaller as they get closer to the bottom of the page using for loops and map: every time the y axis goes down, the spacing and scale of raindrops become smaller. 
 
-~~strikethrough text~~
+I also added an animation that when the user clicks their mouses, the raindrops all drop down the screen using yTransform and if function: if the mouse is pressed, yTransform will keep getting bigger and make the y axis of raindrops keep going down.
 
-Ordered List:
-1. First item
-2. Second item
-3. Third item
-
-Unordered List:
-- First item
-- Second item
-- Third item
-
-`short code block`
-
-```
-extended code block
-fun() {
-  return 0
-}
-```
-
-Link:  
-[linked text](https://www.example.com)
-
-
-Image with url:  
-![image description](https://dm-gy-6063-2024f-b.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
-
-
-Image on repo:  
-![image description](./file-name.jpg)
-
-
-To start a new line, add two spaces at the end of a line, like this:  
-this is a new line.
-
-
-To start a new paragraph, leave an empty line between two lines of text.
-
-This is a new paragraph.
+After that, I gave the stroke of the raindop a random color every time the page refreshes using random colors. Then, I realized that if I put it in the draw {}function, the color will keep changing, which makes it to flash very quick, and if I use noLoop(), the raindrop dropping effect will also go away. Therefore, I set up a strokeHasColor function and an if/else function to make sure the random color will only be assigned to the strokes when strokeHasColor == false, and after that strokeHasColor will become true, and the color assignment stops. 
--- assignment2/sketch.js
+++ assignment2/sketch.js
@@ -1,17 +1,16 @@
 let yTransform = 0;
 // set y position variable for the dropping effect of raindrops
-let fillR;
-let fillG;
-let fillB;
-// set color variables for stroke fills
-let fillHasColor = false;
-// let stroke only be filled once
+let strokeFillR;
+let strokeFillG;
+let strokeFillB;
+// set color variables for stroke color
+let strokeHasColor = false;
+// let stroke only be colored once
       
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
 }
 
 
@@ -19,41 +18,38 @@ function setup() {
 function draw() {
   background("#5b88adff");
   //set background color
+  
 
-  //draw different columns of raindrops
+  //draw columns of raindrops to fill the screen vertically
   for (let y = 20; y < height - 50; y += 100) {
-    // the loop starts at y = 50;
+    // the loop starts at y = 20;
     // for every loop when y < height-50, add y by 100
     let rainDropSpacing = map(y,50,height-50,100,40);
     // make spacing smaller as y-axis goes down
-    
-    //draw different rows of raindrops
+
+    //draw rows of raindrops to fill the screen horizontally
     for (let x = 50; x < width; x += rainDropSpacing) {
       // the for loop starts at x = 50;
-      // for every loop when x < width, add x by the spacing amount
-
+      // for every loop when x < width, add x by the spacing value
+      // the spacing value become smaller as it goes further down the screen
+      // meaning that there will be more horizontal raindrops at the buttom
+      
 
       push();
-      //everything within this push/pop block will be centered with 0,0 as the center point
       translate(x, y); 
-      let rainDropScale = map(y,50,height-50,1,0.25);
-      scale(rainDropScale); 
-      // make raindrop scale smaller as y-axis goes down
-
+      //everything within this push/pop block will be centered with x,y as the center point
       
-      // set random color for raindrop stroke
-      if (fillHasColor == false) {
-        fillR = random(255);
-        fillG = random(255);
-        fillB = random(255); // give stroke random R, G, and B variables
-        fillHasColor = true; // stroke already has a color, let random stop (let the color stays)
-      }
+
+      let rainDropScale = map(y,50,height-50,1,0.25);
+      // make the variable rainDropScale smaller as y-axis goes down
+      scale(rainDropScale); 
+      // assign the variable value to the scale of rain drops
 
 
-      //make the raindrop drop when mouse is pressed
+      //create the dropping effect when mouse is pressed
       if (mouseIsPressed) {
         yTransform = yTransform+0.05; 
-        // adds 0.01 to yTransform every frame
+        // adds 0.05 to yTransform every frame
         translate(0, yTransform); 
         // translates downwards by yTransform
       } 
@@ -62,22 +58,34 @@ function draw() {
         // lifting the mouse button up resets yTransform
       }
 
-
-
-
-      // set stroke information for raindrops
-      strokeWeight(2);
-      stroke(fillR, fillG, fillB, 200);
-      fill('rgba(255, 255, 255, 0.67)');
       
+      // set random color for raindrop stroke
+      if (strokeHasColor == false) {
+        strokeFillR = random(255);
+        strokeFillG = random(255);
+        strokeFillB = random(255); 
+        // give the stroke random R, G, and B color variables
+        strokeHasColor = true; 
+        // when stroke already has a color, let random stop (let the color stays)
+      }
+      // every time the system runs(or page refreshes), the stroke color changes
 
 
-      //draw raindrops
+      // draw raindrop strokes
+      strokeWeight(2); // set stroke weight
+      stroke(strokeFillR, strokeFillG, strokeFillB, 200);
+      // set the random color for the stroke, give it a bit of transparency
+
+      
+      fill('rgba(255, 255, 255, 0.67)');
+      // give a color to the raindrops
+
+      //draw raindrops' shape
       beginShape();
-      vertex(0, 50);
-      vertex(25, 0);
-      vertex(50, 50);
-      endShape(); //tip of the raindrop
+      vertex(0, 50); //tip of the rain drop: left
+      vertex(25, 0); //tip of the rain drop: top
+      vertex(50, 50); //tip of the rain drop: right
+      endShape(); 
       arc(25, 50, 50, 50, radians(0), radians(180)); //bottom of the raindrop
 
 
